8cadb72fa2544eb7c46eb2afb7e57edc2dd6d661
Lars Froehlich <lars.froehlich@online.de>
2020-11-07 22:56:03 +0100

    completed worker thread and commented heavily

--- python/plugins/henningsPlugin/henningPlugin_functions.py
+++ python/plugins/henningsPlugin/henningPlugin_functions.py
@@ -15,17 +15,19 @@
  **********************************************************************************************************************
 """
 
+# QT and QGIS imports
 from PyQt5 import uic
-from PyQt5.QtGui import QValidator
-from PyQt5.QtCore import *
 from PyQt5.QtWidgets import *
 from qgis.utils import iface
-import os							# access files
-import json							# load JSON as a dictionary
-from glob import glob				# access files with wildcards
-from functools import partial		# signal-connect to a function with arguments
-from .henningPlugin_validator import PathValidator
-from .henningPlugin_processor import Worker
+
+# misc. imports
+import os							                    # access files, folders etc
+from functools import partial		                    # signal-connect to a function with arguments
+
+# internal imports
+from .henningPlugin_validator import PathValidator      # subclass of QValidator to check paths of line edit fields
+from .henningPlugin_processor import Worker             # QThread Worker class to get the processing done
+
 
 """This loads your .ui file so that PyQt can populate your plugin with the elements from Qt Designer"""
 FORM_CLASS, _ = uic.loadUiType(os.path.join(os.path.dirname(__file__), 'henningsPlugin.ui'))
@@ -33,40 +35,53 @@ FORM_CLASS, _ = uic.loadUiType(os.path.join(os.path.dirname(__file__), 'hennings
 class DialogAndFunctions(QDialog, FORM_CLASS):
     """Constructor"""
     def __init__(self, parent=None):
-        # Load QT-Designer file (ui) with FORM_CLASS. Using self.setupUi()makes eveery QT element popular -------------
+        # Load QT-Designer file (ui) with FORM_CLASS. Using self.setupUi()makes every QT element popular -------------
         super(DialogAndFunctions, self).__init__(parent)
         self.setupUi(self)
         
         # Variables ---------------------------------------------------------------------------------------------------
+        self.inputDir = None    # input path
+        self.outputDir = None   # output path
 
-        # Initialize the path validator
+        # Init list widget
+        self.listWidget.addItem("Willkommen zu Hennings Plugin!")
+        self.listWidget.addItem("Bitte Input- und Output-Ordner eingeben. Anschließend auf \"Start Prozessierung\" klicken")
+        self.listWidget.scrollToBottom()
+
+        # Initialize progress bar ------------------------------------------------------------------------------------
+        self.progressBar.setValue(0)    # set value to 0 %
+
+        # Initialize the path validator ------------------------------------------------------------------------------
         self.pathValidator = PathValidator()                    # Invoke QValidator Subclass to check for paths
-        self.lineEditInputDir.setValidator(self.pathValidator)  # Set validator of line edit fields
-        self.lineEditOutputDir.setValidator(self.pathValidator)
+        self.lineEditInputDir.setValidator(self.pathValidator)  # Set validator of lineeditobject
+        self.lineEditOutputDir.setValidator(self.pathValidator) # Set validator of lineeditobject
 
-        # Connect QT elements with functions ----------------------------------------------------------------------
-        self.buttonBox.accepted.connect(self.startProcessing)       # OK-Button
-        self.buttonBox.rejected.connect(self.cancel)                # Cancel-Button
-        self.buttonInputDir.clicked.connect(partial(self.chooseDirectory, self.buttonInputDir, self.lineEditInputDir))           # Input Directory Button
-        self.buttonOutputDir.clicked.connect(partial(self.chooseDirectory, self.buttonOutputDir, self.lineEditOutputDir))        # Output Directory Button
-        self.lineEditInputDir.textChanged.connect(partial(self.checkPath, self.lineEditInputDir))
-        self.lineEditOutputDir.textChanged.connect(partial(self.checkPath, self.lineEditOutputDir))
+        # Connect QT elements with functions -------------------------------------------------------------------------
+        self.startButton.clicked.connect(self.startProcessing)       # Connect Start Prozessierung-Button with method startProcessing
+        self.buttonBox.accepted.connect(self.ok)                     # Connect OK-Button with method ok
+        self.buttonBox.rejected.connect(self.cancel)                 # Connect Cancel-Button with method cancel
+        self.buttonInputDir.clicked.connect(partial(self.chooseDirectory, self.buttonInputDir, self.lineEditInputDir))       # Connect Input Directory Button with chooseDirectory method
+        self.buttonOutputDir.clicked.connect(partial(self.chooseDirectory, self.buttonOutputDir, self.lineEditOutputDir))    # Connect Output Directory Button with chooseDirectory method
+        self.lineEditInputDir.textChanged.connect(partial(self.checkPath, self.lineEditInputDir))     # Connect lineEdit with checkPath method
+        self.lineEditOutputDir.textChanged.connect(partial(self.checkPath, self.lineEditOutputDir))   # Connect lineEdit with checkPath method
 
         # Set initial button states -----------------------------------------------------------------------------------
-        self.buttonBox.button(0x00000400).setEnabled(False)     # OK-Button
-        self.buttonBox.button(0x00400000).setEnabled(True)      # Cancel-Button
+        self.buttonBox.button(0x00000400).setEnabled(False)         # disable OK-Button
+        self.buttonBox.button(0x00400000).setEnabled(False)         # disable Cancel-Button
+        self.startButton.setEnabled(False)                          # disable Start Prozessierung-Button
 
     """One of the content elements was edited: font color depends on existence of path """
     def checkPath(self, lineEdit):
-        lineEdit.setValidator(self.pathValidator)       # validate path
-        color = "Black" if lineEdit.hasAcceptableInput() else "Red"
-        lineEdit.setStyleSheet("font:9pt \"Arial\";color:%s" % color)
+        color = "Black" if lineEdit.hasAcceptableInput() else "Red"     # check input / output paths and color them accordingly
+        lineEdit.setStyleSheet("font:9pt \"Arial\";color:%s" % color)   # apply the style to the lineEdit object
 
-        # Enable OK-Button if all paths exist
+        # Enable Start Prozessierung-Button if all paths exist
         if self.lineEditInputDir.hasAcceptableInput() and self.lineEditOutputDir.hasAcceptableInput():
-            self.buttonBox.button(0x00000400).setEnabled(True)  # enable OK-Button
+            self.startButton.setEnabled(True)                   # enable Prozessierung-Button
+            self.inputDir = self.lineEditInputDir.text()        # set inputDir var from current lineEdit-text
+            self.outputDir = self.lineEditOutputDir.text()      # set outputDir var from current lineEdit-text
         else:
-            self.buttonBox.button(0x00000400).setEnabled(False)  # disable OK-Button
+            self.startButton.setEnabled(False)                   # disable Prozessierung-Button
 
     """...-button clicked: user may select another path """
     def chooseDirectory(self, pushButton, lineEdit):
@@ -77,17 +92,66 @@ class DialogAndFunctions(QDialog, FORM_CLASS):
         lineEdit.setStyleSheet("color:Black")
         lineEdit.setText(unicode(path))
 
-    """OK button clicked: store elements in the property file and quit plugin """
+    """Worker-Thread sends a dictionary: refresh Messages, Log, Progress Bar and Button-states """
+    def refreshMessagingButtonsProgress(self, dct_):
+
+        # button handling
+        if dct_['okButtonEnabled'] == True:
+            self.buttonBox.button(0x00000400).setEnabled(True)    # enable OK-Button as soon as processing is finished
+        if dct_['okButtonEnabled'] == False:
+            self.buttonBox.button(0x00000400).setEnabled(False)
+        if dct_['cancelButtonEnabled'] == True:
+            self.buttonBox.button(0x00400000).setEnabled(True)      # enable Cancel-Button
+        if dct_['cancelButtonEnabled'] == False:
+            self.buttonBox.button(0x00400000).setEnabled(False)     # disable Cancel-Button
+
+        # message handling here:
+        if dct_['message']:
+            self.listWidget.addItem(dct_['message'])
+            self.listWidget.scrollToBottom()
+            pass
+
+        # Set progress bar
+        self.progressBar.setValue(dct_['count'])
+
+        # insert logging handling here
+        if dct_['log'] == True:
+            #log(self.v['logFile'], dct_['plugin'], dct_['level'], dct_['message'])
+            pass
+
+        # insert handling if process was a success:
+        if dct_['level'] == 'SUC':
+            iface.messageBar().pushSuccess("Hennings Plugin", dct_['message'])
+
+        dct_.clear()  # empty the dict
+
+        return
+
+
+    """Start Prozessierung-button clicked: start the Worker-thread and collect signals from thread """
     def startProcessing(self):
-        iface.messageBar().pushMessage("Hennings Plugin", "Prozessierung startet...")
-        self.buttonBox.button(0x00000400).setEnabled(False)     # OK-Button
+        self.listWidget.addItem("Prozessierung startet...")
+        self.startButton.setEnabled(False)                      # Start Prozessierung-Button
 
-        self.job = Worker()
-        self.job.dctMessages.connect()  # Connect the signal dictionary
+        # Initialize worker thread ------------------------------------------------------------------------------------
+        self.job = Worker(self.inputDir, self.outputDir)
+        self.job.dctMessages.connect(self.refreshMessagingButtonsProgress)  # Connect the signal dictionary
 
-        self.job.start()
+        self.job.start()    # Start the worker thread
 
-    """Cancel button clicked: quit without storing """
+        return
+
+
+    """Cancel button clicked: quit processing thread and close window """
     def cancel(self):
-        iface.messageBar().pushWarning("Hennings Plugin", "Prozessierung Abgebrochen")
-        self.close()
+        try:
+            self.job.terminate()
+            iface.messageBar().pushWarning("Hennings Plugin", "Prozessierung Abgebrochen")
+        except:
+            pass
+
+        self.close()    # close window
+
+    """OK button clicked: close window """
+    def ok(self):
+        self.close()    # close window
--- python/plugins/henningsPlugin/henningPlugin_processor.py
+++ python/plugins/henningsPlugin/henningPlugin_processor.py
@@ -1,11 +1,11 @@
 # -*- coding: utf-8 -*-
 """
  **********************************************************************************************************************
-	alkisLoader		A QGIS 3 plugin
+	henningPlugin	A QGIS 3 plugin
 
  ----------------------------------------------------------------------------------------------------------------------
 	copyright:		(C) 2020 by Lars Froehlich
-	email:			lars.froehlich@lgln.niedersachsen.de
+	email:			lars.froehlich@online.de
  ----------------------------------------------------------------------------------------------------------------------
 
  **********************************************************************************************************************
@@ -16,7 +16,7 @@
 """
 
 from PyQt5.QtCore import *
-from qgis.core import *
+import time
 
 """********************************************************************************************************************
 													Class
@@ -26,9 +26,6 @@ from qgis.core import *
 # Thread-"working"-class:
 class Worker(QThread):
     """
-    Download
-    Extract GZ Alkis package
-
     Info:
     A QThread instance represents a thread and provides the means to start()
     a thread, which will then execute the reimplementation of QThread::run().
@@ -38,26 +35,53 @@ class Worker(QThread):
     # QT-signal-variables for communication from one thread to another
     dctMessages = pyqtSignal(dict)  # Defines a dictionary as pyqt signal variable
 
-    def __init__(self):
+    """Constructor"""
+    def __init__(self, inputDir, outputDir):
         """Refresh the progress bar from dictionary keys current and sum"""
         super().__init__()      # init QThread
 
+        self.inputDir = inputDir
+        self.outputDir = outputDir
+        # define additional variables here ------------------------------------------------------------------------------
 
+    """Helper method to send progress refresh to main thread"""
     def refreshProgress(self, _dct):
         progress = int(_dct['current'] / _dct['sum'] * 100)
         if progress > 100:
             progress = 100
         self.dctMessages.emit({"plugin": "henningPlugin", "level": "INF",
-                               "message": '',
-                               "count": progress, 'log': False})
+                               "message": _dct['message'],
+                               "count": progress, 'log': False,
+                               "okButtonEnabled": False,
+                               "cancelButtonEnabled": True})
 
-    def run(self):  # This is the thread entry point of QThread
 
-        count = 0
+    """This method is the thread entry point of QThread"""
+    def run(self):
+
         self.dctMessages.emit(
-            {"plugin": "henningPlugin", "level": "INF", "message": "INFO: Starte Thread", "count": count, 'log': True})
+            {"plugin": "henningPlugin", "level": "INF",
+             "message": "INFO: Starte Prozessierung. Input-Ordner: %s   Output-Ordner: %s" % (self.inputDir, self.outputDir),
+             "count": 0, 'log': True,
+             'okButtonEnabled': False, "cancelButtonEnabled": True})
 
-        dct = {'sum':100, 'current': 0}
-        for x in range(100):
+        #################################
+        # EXAMPLE #
+        # PUT YOUR SCRIPT HERE
+        #################################
+        dct = {'sum': 100, 'current': 0}
+        for x in range(101):
             dct['current'] = x
+            dct['message'] = "INFO: Prozessiere Karte %s von %s" % (x, dct['sum'])
             self.refreshProgress(dct)
+            time.sleep(0.05)
+        # ###############################
+        # EXAMPLE END
+        #################################
+
+        self.dctMessages.emit(
+            {"plugin": "henningPlugin", "level": "SUC",
+             "message": "INFO: Prozessierung fertig! Die Karten sind in Ordner %s gespeichert." % self.outputDir, "count": 100, 'log': True,
+             'okButtonEnabled': True, "cancelButtonEnabled": False})
+
+        return
